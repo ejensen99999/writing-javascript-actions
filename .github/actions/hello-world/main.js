e883a307144329cf6a2b343208a68ba8c1c190af
Eric Jensen <eric.jensen2@extendhealth.com>
2022-05-10 10:06:11 -0600

    add main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+var core = require ("@actions/core")
+
+var firstG = core.getInput("first-greeting")
+var secondG = core.getInput("second-greeting")
+
+console.log(`Hello ${firstG}`);
+
+if(secondG)
+{
+    console.log(`Hello ${secondG}`)
+}
